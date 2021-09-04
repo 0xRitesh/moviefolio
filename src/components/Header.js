dc7aec31754742cd995a688be2d8a1c5ef670d59
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-09-04 11:56:22 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function header() {
+    return (
+        <header className =''>
+            <h1>Demo header</h1>
+        <image 
+        className="object-contain"
+        src="https://i.ibb.co/WKcxQ8f/logo.png" 
+        width={400} height={100} />
+        </header>
+    )
+}
+
+export default header
