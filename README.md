27f988a6de35cbc5b967d1f459c7e5d4f0bcffa9
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-09-24 17:31:30 +0530

    feat : server side rendering

---
+++ README.md
@@ -0,0 +1,17 @@
+# moviefolio
+Responsive Movie Streaming Project built with react
+![]()
+
+> This is a Next.js project bootstrapped with create-next-app.
+
+
+## Technologies Used 🛠️
+- next.js
+- taliwind css
+- rest api
+- tmdb api
+- flex box
+- css grid
+- jit compiler
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <div>
       <Head>
@@ -14,7 +16,25 @@ export default function Home() {
       {/* Header */}
       <Header />
       <Nav />
+      <Results results={results}/>
 
     </div>
   )
+}
+
+//Server side render
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
